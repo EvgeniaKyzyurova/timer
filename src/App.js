92b39ff612b5c32cf69599306251e585534b3f95
Kyzyurova Evgenia <kyzyurova.evgenia@gmail.com>
2022-02-02 21:03:05 +0200

    timer on React with rxjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
+import {interval, Subject} from "rxjs";
+import {takeUntil} from "rxjs/operators";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [time, setTime] = useState(0);
+    const [watchOn, setWatchOn] = useState(false);
+    const [state, setState] = useState('start');
+
+    useEffect(() => {
+        !watchOn ? setState('start') : setState('stop');
+
+        const unsubscribe$ = new Subject();
+        interval(1000)
+            .pipe(takeUntil(unsubscribe$))
+            .subscribe(() => {
+                if (watchOn) {
+                    setTime(val => val + 1);
+                }
+            });
+        return () => {
+            unsubscribe$.next();
+            unsubscribe$.complete();
+        };
+
+    }, [watchOn]);
+
+    const handleStart = () => {
+        setWatchOn(prevState => !prevState);
+        if (state === 'stop') {
+            setTime(0);
+        }
+    }
+    const handleWait = () => {
+        if (time !== 0) {
+            setWatchOn(false)
+        }
+    }
+    const handleReset = () => {
+        setTime(0);
+        setWatchOn(true);
+    }
+
+
+    return (
+        <div className="App">
+            <header className='header'>
+                <div className='container'>
+                    <h1>Timer</h1>
+                    <span className="time">{('0' + Math.floor((time / 3600) % 100)).slice(-2)}</span>:
+                    <span className="time">{('0' + Math.floor((time / 60) % 60)).slice(-2)}</span>:
+                    <span className="time">{('0' + Math.floor(time % 60)).slice(-2)}</span>
+                
+                    <div>
+                    {(state === 'start') ?
+                    <div>
+                        <button onClick={handleStart} className='btn startBtn'>Start</button>
+                        <button onClick={handleWait} className='btn waitBtn'>Wait</button>
+                        <button onClick={handleReset} className='btn resetBtn'>Reset</button>
+                    </div>: ""
+                    }
+                    {(state === 'stop') ?
+                    <div>
+                        <button onClick={handleStart} className='btn stopBtn'>Stop</button>
+                        <button onClick={handleWait} className='btn waitBtn'>Wait</button>
+                        <button onClick={handleReset} className='btn resetBtn'>Reset</button>
+                    </div>: ""
+                    }
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
